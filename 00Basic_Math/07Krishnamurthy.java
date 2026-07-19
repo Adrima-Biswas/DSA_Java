0679724b37d932639333d567341d77be09b75c9d
Adrima Biswas <adrimabiswas2003@gmail.com>
2026-07-19 16:47:45 +0530

    Update 07Krishnamurthy.java

--- 00Basic_Math/07Krishnamurthy.java
+++ 00Basic_Math/07Krishnamurthy.java
@@ -1,1 +1,25 @@
-
+//krishnamurti
+import java.util.*;
+public class Main {
+    public static boolean krishnamurti(int a){
+        int q = 0;
+        int z = a;
+        while(a != 0){
+            int rem = a % 10;
+            a = a / 10;
+            int p = 1;
+            for(int i = 2; i <= rem; i++){
+                p = p * i;
+            }
+            q = q + p;
+        }
+        return q == z;
+    }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.print("give the number: ");
+		int num = sc.nextInt();
+		boolean ans = krishnamurti(num);
+		System.out.println("The number "+num+" is krishnamurthy: "+ans);
+	}
+}
