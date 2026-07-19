2b7c25475c2ca97789da7cb71d21fc619d9319d1
Adrima Biswas <adrimabiswas2003@gmail.com>
2026-07-19 16:47:12 +0530

    Update 06Palindrome.java

--- 00Basic_Math/06Palindrome.java
+++ 00Basic_Math/06Palindrome.java
@@ -2,25 +2,30 @@
 	Problem Statement: Palindrome Number
 	File Name: 03Palindrome.java
 */
-import java.util.Scanner;
-class Palindrome {
-	private static int reverse(int num) {
-		int rev = 0;
-		while(num != 0) {
-			int rem = num % 10;
-			rev = (rev*10) + rem;
-			num /= 10;
-		}
-		return rev;
-	}
-	private static boolean palindrome(int num) {
-		return num == reverse(num);
-	}
+
+//palindrom 
+import java.util.*;
+public class Main
+{
+    public static boolean palindrom(int num){
+        
+        // if(num < 10) return true;
+        
+        int temp = num;
+        
+        int rev = 0;
+        while(num != 0){
+            int rem = num % 10;
+            num = num / 10;
+            rev = (rev * 10) + rem;
+        }
+        return temp == rev;
+    }
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter Number: ");
-		int num = sc.nextInt();
-		boolean palindrome = palindrome(num);
-		System.out.print(num+" is a Palindrome: "+palindrome);
+	    Scanner sc = new Scanner(System.in);
+		System.out.print("Give the number: ");
+		int n = sc.nextInt();
+		boolean ans = palindrom(n);
+		System.out.println("The number is plaindrome: "+ans);
 	}
 }
